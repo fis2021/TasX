30b65cb4f68b244ff3ba2e605eece998639b28a4
adelabiciusca <adela.biciusca@gmail.com>
2021-05-09 13:59:36 +0300

    Update register form

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -8,7 +8,8 @@ import javafx.stage.Stage;
 import org.dizitart.no2.Nitrite;
 
 public class Main extends Application {
-
+    static boolean admin = false;
+    static String mail = "";
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
